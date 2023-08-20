9e4e280dce59f0ea69be79bc01a1dddf0b0d2150
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-08-19 20:42:03 -0400

    req.params request

--- express_server.js
+++ express_server.js
@@ -23,3 +23,13 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: req.params.longURL};
+  res.render("urls_show", templateVars);
+});
