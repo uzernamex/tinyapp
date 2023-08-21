f477c909f65abad754d1e0ed11fa3c16f91cb138
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-08-20 21:26:39 -0400

    add for loop to iterate through the cnaracters

--- express_server.js
+++ express_server.js
@@ -2,11 +2,23 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 app.set("view engine", "ejs");
+function generateRandomString() {
+  const alphaNumeric = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
+  let randomString = " ";
+  for (let i = 0, i < alphaNumeric.length; i++) {
+   
+  }
+   
+  return 
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -40,12 +52,16 @@ app.get("/urls/:id", (req, res) => {
   }
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id
-  ]};
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
