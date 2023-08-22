b7edb471993203fb5706fa7545f3cfe4876d6d85
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-08-22 19:21:00 -0400

    linted test file

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -4,26 +4,25 @@ const { getUserByEmail } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail("user@example.com", testUsers)
+    const user = getUserByEmail("user@example.com", testUsers);
     const expectedUserID = "userRandomID";
     assert.equal(user.id, expectedUserID, `Expected user ID to be: ${expectedUserID}`);
-  })
-  describe('expectedUserID', function() {
-    it('should be undefined if there is no email', function() {
-      assert.equal(email, !expectedEmail, `The emails are not a match.`);
-    })  
+  });
+  it('should be undefined if there is no email', function() {
+    const user = getUserByEmail("noemail@example.com", testUsers);
+    assert.isUndefined(user, `This email is not found.`);
   });
 });
